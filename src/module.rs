d3e56fda28179693326c64eaa55df7e889002f09
Antoine Chauvin <antoine@aqora.io>
2024-03-13 12:45:40 +0100

    feat: implement extension-module

---
+++ src/module.rs
@@ -0,0 +1,22 @@
+use crate::commands::Cli;
+use clap::Parser;
+use pyo3::prelude::*;
+
+#[pyfunction]
+pub fn main(py: Python<'_>) -> PyResult<()> {
+    let cli = Cli::parse_from(std::env::args().skip(1));
+    pyo3_asyncio::tokio::run(py, async move {
+        if let Err(e) = cli.run().await {
+            eprintln!("{}", e);
+            std::process::exit(1)
+        }
+        Ok(())
+    })?;
+    Ok(())
+}
+
+#[pymodule]
+pub fn aqora_cli(_: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(main, m)?)?;
+    Ok(())
+}
