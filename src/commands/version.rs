c611c3cff8f61fee549acf35c5d705e84289b9dd
Julian Popescu <hi@julian.dev>
2024-03-20 15:38:47 +0100

    get version from pyproject.toml

--- src/commands/version.rs
+++ src/commands/version.rs
@@ -1,9 +1,19 @@
 use pyo3::Python;
 
+const AQORA_PYPROJECT: &str = include_str!("../../pyproject.toml");
+
 lazy_static::lazy_static! {
     static ref VERSION: String = format!(
         "{}\nPython {}",
-        clap::crate_version!(),
+        toml::from_str::<toml::Value>(AQORA_PYPROJECT)
+            .ok()
+            .and_then(|toml| {
+                toml.get("project")
+                    .and_then(|project| project.get("version"))
+                    .and_then(|version| version.as_str())
+                    .map(|version| version.to_string())
+            })
+            .unwrap_or_else(|| clap::crate_version!().to_string()),
         Python::with_gil(|py| py.version().to_string())
     );
 }
