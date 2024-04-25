17df066e35e40b6786345821033a1454434df9ff
Julian Popescu <hi@julian.dev>
2024-04-25 18:31:34 +0200

    fix: pip uninstall removed deps

--- src/commands/remove.rs
+++ src/commands/remove.rs
@@ -3,7 +3,7 @@ use crate::{
     commands::GlobalArgs,
     dirs::{init_venv, pyproject_path, read_pyproject},
     error::{self, Result},
-    python::pip_install,
+    python::{pip_install, pip_uninstall},
     revert_file::RevertFile,
 };
 use aqora_config::{PackageName, Requirement};
@@ -81,69 +81,64 @@ pub async fn remove(args: Remove, global: GlobalArgs) -> Result<()> {
     let mut toml = fs::read_to_string(&project_file)
         .await?
         .parse::<DocumentMut>()?;
-    let project = if let Some(project) = toml.get_mut("project") {
-        project
-    } else {
-        progress.finish_with_message("No dependencies to remove");
-        return Ok(());
-    };
-    let dependencies = if let Some(dependencies) = project
-        .as_table_mut()
-        .ok_or_else(|| {
-            error::user(
-                "Invalid pyproject.toml",
-                "The 'project' section must be a table",
-            )
-        })?
-        .get_mut("dependencies")
-    {
-        dependencies
-    } else {
-        progress.finish_with_message("No dependencies to remove");
-        return Ok(());
-    };
-    let dependencies = dependencies.as_array_mut().ok_or_else(|| {
-        error::user(
-            "Invalid pyproject.toml",
-            "The 'dependencies' section must be an array",
-        )
-    })?;
-    let mut removed_deps = Vec::new();
-    for dep in deps.iter() {
-        removed_deps.extend(remove_matching_dependencies(dependencies, dep)?);
+    let dependencies = toml
+        .get_mut("project")
+        .map(|project| {
+            project.as_table_mut().ok_or_else(|| {
+                error::user(
+                    "Invalid pyproject.toml",
+                    "The 'project' section must be a table",
+                )
+            })
+        })
+        .transpose()?
+        .and_then(|project| project.get_mut("dependencies"))
+        .map(|dependencies| {
+            dependencies.as_array_mut().ok_or_else(|| {
+                error::user(
+                    "Invalid pyproject.toml",
+                    "The 'dependencies' section must be an array",
+                )
+            })
+        })
+        .transpose()?;
+    if let Some(dependencies) = dependencies {
+        let mut removed_deps = Vec::new();
+        for dep in deps.iter() {
+            removed_deps.extend(remove_matching_dependencies(dependencies, dep)?);
+        }
+        if !removed_deps.is_empty() {
+            fs::write(&project_file, toml.to_string())
+                .await
+                .map_err(|e| {
+                    error::user(
+                        &format!("Failed to write pyproject.toml: {e}"),
+                        &format!(
+                            "Make sure you have permissions to write to {}",
+                            project_file.as_ref().display()
+                        ),
+                    )
+                })?;
+        }
     }
-    if removed_deps.is_empty() {
-        progress.finish_with_message("No dependencies to remove");
-        return Ok(());
-    }
-    fs::write(&project_file, toml.to_string())
-        .await
-        .map_err(|e| {
-            error::user(
-                &format!("Failed to write pyproject.toml: {e}"),
-                &format!(
-                    "Make sure you have permissions to write to {}",
-                    project_file.as_ref().display()
-                ),
-            )
-        })?;
+    let pip_options = PipOptions {
+        color: global.color.pip(),
+        ..Default::default()
+    };
+    pip_uninstall(&env, deps.clone(), &pip_options, &progress).await?;
     pip_install(
         &env,
         [PipPackage::editable(&global.project)],
-        &PipOptions {
-            color: global.color.pip(),
-            ..Default::default()
-        },
+        &pip_options,
         &progress,
     )
     .await?;
     project_file
         .commit()
         .map_err(|err| error::system(&format!("Failed to save pyproject.toml: {err}"), ""))?;
-    removed_deps.dedup();
-    let removed_deps = removed_deps
+    let removed_deps = deps
         .iter()
-        .map(|dep| format!("'{dep}'"))
+        .map(|dep| dep.to_string())
         .collect::<Vec<_>>()
         .join(", ");
     progress.finish_with_message(format!("Removed dependencies: {removed_deps}"));
