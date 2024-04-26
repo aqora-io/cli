cd3ae715a8482cac3e4e90bf16f2521a154d7d92
Antoine Chauvin <antoine@aqora.io>
2024-04-26 16:53:34 +0200

    build: fail build on malformed sentry dsn

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
+use sentry_types::Dsn;
+use std::{env, str::FromStr};
 use toml_edit::DocumentMut;
 
 fn main() {
-    println!("cargo:rerun-if-env-changed=SENTRY_DSN");
     println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
     let version = std::env::var("CARGO_PKG_VERSION").unwrap();
     let mut document = std::fs::read_to_string("pyproject.toml")
@@ -14,4 +15,15 @@ fn main() {
         .get_mut("version")
         .unwrap() = toml_edit::value(version);
     std::fs::write("pyproject.toml", document.to_string()).unwrap();
+
+    println!("cargo:rerun-if-env-changed=SENTRY_DSN");
+    match env::var("SENTRY_DSN") {
+        Ok(sentry_dsn) => {
+            if let Err(dsn_error) = Dsn::from_str(&sentry_dsn) {
+                panic!("Malformed Sentry DSN {sentry_dsn:?}: {dsn_error:?}");
+            }
+        }
+        Err(env::VarError::NotPresent) => {}
+        Err(error) => panic!("Unexpected Sentry DSN: {error:?}"),
+    }
 }
