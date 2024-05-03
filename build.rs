34e6304a2ea1d88bec2bff20c8eeb16eccfaecda
Julian Popescu <hi@julian.dev>
2024-05-03 15:20:02 +0200

    Use deps from aqora-cli extras and sync pyproject version

---
+++ build.rs
@@ -0,0 +1,16 @@
+use toml_edit::DocumentMut;
+
+fn main() {
+    println!("cargo:rerun-if-env-changed=CARGO_PKG_VERSION");
+    let version = std::env::var("CARGO_PKG_VERSION").unwrap();
+    let mut document = std::fs::read_to_string("pyproject.toml")
+        .unwrap()
+        .parse::<DocumentMut>()
+        .unwrap();
+    *document
+        .get_mut("project")
+        .unwrap()
+        .get_mut("version")
+        .unwrap() = toml_edit::value(version);
+    std::fs::write("pyproject.toml", document.to_string()).unwrap();
+}
