0477ef290fa1c4977df8c85a043f3761c8c8f116
Antoine Chauvin <antoine@aqora.io>
2024-05-23 17:38:40 +0200

    feat: show decompress progression bar

--- src/progress_bar.rs
+++ src/progress_bar.rs
@@ -3,6 +3,17 @@ use std::fmt::Write;
 
 pub fn pretty() -> ProgressStyle {
     ProgressStyle::with_template(
+        "{spinner} [{elapsed_precise}] {msg} [{wide_bar}] {pos:>7}/{len:7} ({eta})",
+    )
+    .unwrap()
+    .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+        write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+    })
+    .progress_chars("#>-")
+}
+
+pub fn pretty_bytes() -> ProgressStyle {
+    ProgressStyle::with_template(
         "{spinner} [{elapsed_precise}] {msg} [{wide_bar}] {bytes}/{total_bytes} ({eta})",
     )
     .unwrap()
