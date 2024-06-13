7cdd3d55252d61f867ee2af246a09e88d8d1dab1
Julian Popescu <hi@julian.dev>
2024-06-13 13:54:43 +0200

    fix: fix versioning by using cargo.toml version

--- src/manifest.rs
+++ src/manifest.rs
@@ -2,6 +2,7 @@ use aqora_config::{pyproject_toml, Version};
 
 lazy_static::lazy_static! {
     pub static ref MANIFEST: pyproject_toml::PyProjectToml = toml::from_str(include_str!("../pyproject.toml")).unwrap();
+    pub static ref CARGO_PKG_VERSION: Version = env!("CARGO_PKG_VERSION").parse().unwrap();
 }
 
 pub fn manifest_version() -> &'static Version {
@@ -9,7 +10,7 @@ pub fn manifest_version() -> &'static Version {
         .project
         .as_ref()
         .and_then(|project| project.version.as_ref())
-        .unwrap()
+        .unwrap_or(&CARGO_PKG_VERSION)
 }
 
 pub fn manifest_name() -> &'static str {
