3f67cbf0a2e2265361936f309c3634ec70147cb1
Angel Dijoux <angel.dijoux@yahoo.com>
2024-06-26 18:24:56 +0200

    feat: operate from 'pyproject.toml'

--- src/commands/lab.rs
+++ src/commands/lab.rs
@@ -1,64 +1,90 @@
-use std::{
-    ffi::OsString,
-    path::{Path, PathBuf},
-    process::Command,
-};
+use std::{ffi::OsString, path::PathBuf, process::Command};
 
 use clap::Args;
 use serde::Serialize;
 
-use crate::error::Result;
+use crate::{
+    dirs::read_pyproject,
+    error::{self, Result},
+};
 
 use super::GlobalArgs;
 
 use crate::commands::python::{python, Python};
 
-fn activate_virtual_env(virtual_env_path: &Path) -> Result<(), std::io::Error> {
-    let activate_script = if cfg!(windows) {
-        virtual_env_path.join("Scripts").join("activate.bat")
-    } else {
-        virtual_env_path.join("bin").join("activate")
-    };
+const VS_CODE_NOT_FOUND_MSG: &str = "VS Code not found. You can install it from https://code.visualstudio.com/ or run `aqora lab -j` to open the lab without VS Code.";
 
-    let cmd = if cfg!(windows) {
-        format!("\"{}\"", activate_script.to_str().unwrap())
-    } else {
-        format!("source {}", activate_script.to_str().unwrap())
-    };
+fn is_vscode_available() -> Result<(), String> {
+    match Command::new("code").arg("--version").output() {
+        Ok(output) => {
+            if output.status.success() {
+                Ok(())
+            } else {
+                Err(VS_CODE_NOT_FOUND_MSG.to_string())
+            }
+        }
+        Err(_) => Err(VS_CODE_NOT_FOUND_MSG.to_string()),
+    }
+}
 
-    let output = Command::new("sh").arg("-c").arg(&cmd).output()?;
+fn install_extensions() -> Result<()> {
+    let extensions = vec!["ms-python.python", "ms-toolsai.jupyter"];
 
-    println!("{}", String::from_utf8_lossy(&output.stdout));
+    for extension in extensions {
+        Command::new("code")
+            .args(["--install-extension", extension, "--force"])
+            .spawn()?
+            .wait()?;
+    }
 
     Ok(())
 }
 
-fn install_extensions() -> Result<(), std::io::Error> {
-    let python_extension = "ms-python.python";
-    Command::new("code")
-        .args(["--install-extension", python_extension, "--force"])
-        .spawn()?
-        .wait()?;
+fn open_vscode(path: PathBuf, module: String, name: String) -> Result<(), std::io::Error> {
+    let notebook_path = format!("{}/{}/{}.ipynb", path.display(), module, name);
+    run_vscode_with_args(&[
+        path.display().to_string(),
+        "--goto".to_string(),
+        notebook_path,
+    ])
+}
 
-    let jupyter_extension = "ms-toolsai.jupyter";
-    Command::new("code")
-        .args(["--install-extension", jupyter_extension, "--force"])
-        .spawn()?
-        .wait()?;
+fn open_vscode_pyproject(path: PathBuf) -> Result<(), std::io::Error> {
+    let toml_path = path.join("pyproject.toml");
+    run_vscode_with_args(&[toml_path.display().to_string()])
+}
 
+fn run_vscode_with_args(args: &[String]) -> Result<(), std::io::Error> {
+    Command::new("code").args(args).spawn()?.wait()?;
     Ok(())
 }
 
-fn open_vscode(path: PathBuf) -> Result<(), std::io::Error> {
-    let path_with_submission = format!("{}", path.display());
-    Command::new("code")
-        .args(&[
-            path_with_submission.clone(),
-            "--goto".to_string(),
-            format!("{}/submission/notebook.ipynb:1:1", path_with_submission),
-        ])
-        .spawn()?
-        .wait()?;
+async fn handle_vscode_integration(global_args: GlobalArgs) -> Result<()> {
+    is_vscode_available().map_err(|err_msg| error::user("vscode not found 😞", &err_msg))?;
+
+    install_extensions()?;
+
+    let project = read_pyproject(&global_args.project).await?;
+    let aqora = project.aqora().ok_or_else(|| {
+        error::user(
+            "No [tool.aqora] section found in pyproject.toml",
+            "Please make sure you are in the correct directory",
+        )
+    })?;
+
+    if let Some(submission) = aqora.as_submission() {
+        if let Some((_key, function_def)) = submission.refs.iter().next() {
+            let path = function_def.path.clone();
+            open_vscode(
+                global_args.project,
+                path.module().to_string(),
+                path.name().to_string(),
+            )?;
+        } else {
+            open_vscode_pyproject(global_args.project)?;
+        }
+    }
+
     Ok(())
 }
 
@@ -71,10 +97,7 @@ pub struct Lab {
 
 pub async fn lab(args: Lab, global_args: GlobalArgs) -> Result<()> {
     if !args.jupyter_notebook {
-        install_extensions()?;
-        activate_virtual_env(&global_args.project.join(".venv"))?;
-        open_vscode(global_args.project)?;
-        Ok(())
+        handle_vscode_integration(global_args).await
     } else {
         let args = Python {
             module: Some("jupyterlab".into()),
