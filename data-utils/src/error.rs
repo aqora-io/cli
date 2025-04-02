3cd9e6fe0a12415b7f209ec2bd29a89287cc0d27
Julian Popescu <hi@julian.dev>
2025-04-02 14:06:32 +0200

    feat: data utils
    implements utilities for inferring schemas and converting CSVs and JSONs
    to parquet files

---
+++ data-utils/src/error.rs
@@ -0,0 +1,25 @@
+use std::convert::Infallible;
+use std::io;
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    SerdeArrow(#[from] serde_arrow::Error),
+    #[error(transparent)]
+    Parquet(#[from] parquet::errors::ParquetError),
+    #[cfg(feature = "wasm")]
+    #[error("JavaScript error")]
+    Js(wasm_bindgen::JsValue),
+}
+
+impl From<Infallible> for Error {
+    fn from(err: Infallible) -> Self {
+        match err {}
+    }
+}
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
