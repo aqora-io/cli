e30adf5971a14780d20ee652c2d9a15c56df4548
Antoine Chauvin <antoine@aqora.io>
2025-04-23 15:16:32 +0200

    feat: query Competition::show_metric

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -125,7 +125,7 @@ pub async fn install_submission(
     write_project_config_default(
         &global.project,
         &ProjectConfig {
-            show_score: competition.has_leaderboard,
+            show_score: competition.show_metric,
         },
     )
     .await?;
