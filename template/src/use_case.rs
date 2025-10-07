65da805968e8d457929626ae6d87270feb70e6a1
Julian Popescu <hi@julian.dev>
2025-10-07 13:20:41 +0200

    feat: add template for kubimo

--- template/src/use_case.rs
+++ template/src/use_case.rs
@@ -2,10 +2,10 @@ use std::path::Path;
 
 use derive_builder::Builder;
 use handlebars::{RenderError, RenderErrorReason};
-use regex::Regex;
 use serde::Serialize;
 
 use crate::registry::REGISTRY;
+use crate::utils::{assert_no_control_chars, assert_semver, assert_slug};
 
 const DEFAULT_PYTHON_VERSION: &str = "3.8";
 const DEFAULT_CLI_VERSION_STR: &str = env!("CARGO_PKG_VERSION");
@@ -35,29 +35,13 @@ impl UseCaseTemplate {
 
 impl UseCaseTemplateBuilder {
     fn validate(&self) -> Result<(), String> {
-        lazy_static::lazy_static! {
-            static ref SEMVER_REGEX: Regex = Regex::new(r"^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$").unwrap();
-            static ref SLUG_REGEX: Regex = Regex::new(r"^[-a-zA-Z0-9_]*$").unwrap();
-        }
-
-        if let Some(python_version) = self.python_version.as_ref() {
-            if !SEMVER_REGEX.is_match(python_version) {
-                return Err(format!("Invalid Python version: {}", python_version));
-            }
-        }
-        if let Some(cli_version) = self.cli_version.as_ref() {
-            if !SEMVER_REGEX.is_match(cli_version) {
-                return Err(format!("Invalid CLI version: {}", cli_version));
-            }
-        }
-        let title = self.title.as_ref().ok_or("Title is required")?;
-        if title.contains(|c: char| c.is_control()) {
-            return Err("Title must not contain control characters".to_string());
-        }
-        let competition = self.competition.as_ref().ok_or("Competition is required")?;
-        if !SLUG_REGEX.is_match(competition) {
-            return Err("Competition must be a valid slug".to_string());
-        }
+        self.python_version
+            .as_deref()
+            .map(assert_semver)
+            .transpose()?;
+        self.cli_version.as_deref().map(assert_semver).transpose()?;
+        assert_no_control_chars(self.title.as_ref().ok_or("Title is required")?)?;
+        assert_slug(self.competition.as_ref().ok_or("Competition is required")?)?;
         Ok(())
     }
 
