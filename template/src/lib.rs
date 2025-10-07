b9b1fec4285bcba73dce26c0e994bfa4f0e43cfb
Julian Popescu <hi@julian.dev>
2025-10-07 13:20:41 +0200

    feat: add template for kubimo

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,10 @@
+pub mod dataset_marimo;
 pub mod registry;
 pub mod use_case;
+pub mod utils;
 
 pub use handlebars;
 
+pub use dataset_marimo::DatasetMarimoTemplate;
 pub use handlebars::RenderError;
 pub use use_case::UseCaseTemplate;
