9a4e24b2e3830797b7cc6848eb7947deb3ed1c16
Angel-Dijoux <angel.dijoux@yahoo.com>
2025-10-27 12:34:49 +0900

    feat: reset dataset version draft

--- src/commands/dataset/version/mod.rs
+++ src/commands/dataset/version/mod.rs
@@ -1,6 +1,7 @@
 pub mod common;
 pub mod list;
 pub mod new;
+pub mod reset;
 
 use crate::{commands::GlobalArgs, error::Result};
 use clap::Subcommand;
@@ -8,16 +9,19 @@ use serde::Serialize;
 
 use list::{list, List};
 use new::{new, New};
+use reset::{reset, Reset};
 
 #[derive(Subcommand, Debug, Serialize)]
 pub enum Version {
     New(New),
     List(List),
+    Reset(Reset),
 }
 
 pub async fn version(args: Version, global: GlobalArgs) -> Result<()> {
     match args {
         Version::New(args) => new(args, global).await,
         Version::List(args) => list(args, global).await,
+        Version::Reset(args) => reset(args, global).await,
     }
 }
