dcee55eaa1a60e98bc66f65f833471bc46df470a
Julian Popescu <hi@julian.dev>
2026-04-24 13:31:26 +0200

    fix: rename notebook to load

--- python/aqora/__init__.py
+++ python/aqora/__init__.py
@@ -1,7 +1,7 @@
 from typing_extensions import Any, TypedDict, override
 
 from . import _aqora
-from .notebook import notebook
+from .notebook import load
 from aqora._aqora import *  # pyright: ignore[reportAssignmentType, reportWildcardImportFromLibrary]  # noqa: F403
 
 
